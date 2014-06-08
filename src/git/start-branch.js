2fb45e8f4084a188ea2dfdf78fb287383973930a
cmaples <cmaples@singularity-interactive.com>
2014-06-08 00:57:45 -0600

    start branch script has a description at the beginning now

--- src/git/start-branch.js
+++ src/git/start-branch.js
@@ -10,12 +10,19 @@ Console.InputHandlers.StdIn = {
         Console.Exit(data || 0);
     },
     Start : function(data) {
-        Console.WriteLine("What is your ticket number?");
+        Console.WriteLine("This application will create a new branch based on a ticket identifier (number) and ticket name, which allows for the other applications in this suite to do things like commit with the ticket number tagged in the commit message.");
+        Console.WriteLine("");
+        Console.WriteLine("In JIRA, for example, ticket numbers are likely the Epic name (uppercase) and the ticket id, like so: USERS-766.");
+        Console.WriteLine("");
+        Console.WriteLine("This allows for a git workflow that is based on individual tickets.");
+        Console.WriteLine("");
+
+        Console.WriteLine("What is your ticket number? ex. USERS-766");
         Console.HandleInput('StdIn', 'TicketNumber');
     },
     TicketNumber : function(data) {
         Console.Input.TicketNumber = !!data ? Console.cleanInput(data).toUpperCase() : null;
-        Console.WriteLine("What is your ticket name?");
+        Console.WriteLine("What is your ticket name? ex. can edit my own information");
         Console.HandleInput('StdIn', 'TicketName');
     },
     TicketName : function(data) {
