67d42ffeded7db0f028b544e1d92783faf4c6316
cmaples <cmaples@singularity-interactive.com>
2014-08-06 18:20:41 -0600

    master added string capitalization of first letter method

--- src/utils.js
+++ src/utils.js
@@ -233,6 +233,10 @@
         }
     }
 
+    utils.capitaliseFirstLetter = function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
         module.exports = utils;
     } else {
